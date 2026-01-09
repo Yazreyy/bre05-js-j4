60a4ad32de1810367c86955fcd5168f2c3f7ff42
Marlonn Gillet <gillet.marlonn56@icloud.com>
2026-01-09 12:09:40 +0100

    correction

--- exercice-6/index.js
+++ exercice-6/index.js
@@ -1,35 +1,9 @@
-let togglespan = document.querySelectorAll(" h2 span");
+togglespan = document.querySelectorAll(" h2 span");
 togglespan.forEach(span =>{
-    
-span.addEventListener("click" , () => {
-        
-    
-        let titleElement = span.parentElement;
-        
-        
-        let detailSection = titleElement.nextElementSibling; 
-
-        
-        if (detailSection) {
-            
-            
-
-            
-            if (detailSection.classList.contains('closed')) {
-                
-                detailSection.classList.remove('closed');
-                detailSection.classList.add('open');
-                
-                
-                span.textContent = '–'; 
-            } else {
-                
-                detailSection.classList.remove('open');
-                detailSection.classList.add('closed');
-                
-               
-                span.textContent = '+';
-            }
-        }
+    span.addEventListener("click" , () => {
+       let section = document.querySelector("section");
+       
+       section.classList.toggle("open");
+       section.classList.toggle("closed");
     });
 });
